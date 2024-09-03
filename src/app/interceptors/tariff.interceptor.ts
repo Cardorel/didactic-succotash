033ee3eeca3ff8b26d324175bcbfbf5cee145ffe
NgassakiJunescyPRATH <junescy.ngassaki@pratho.com>
2024-09-03 14:40:33 +0200

    modified those files adding env

--- src/app/interceptors/tariff.interceptor.ts
+++ src/app/interceptors/tariff.interceptor.ts
@@ -1,8 +1,5 @@
 import {HttpInterceptorFn} from '@angular/common/http';
 
 export const tariffInterceptor: HttpInterceptorFn = (req, next) => {
-  const newReq = req.clone({
-    headers: req.headers.append('apikey', 'WTqObuahSylAymvvRvEOw71Mmm4r3FOE'),
-  });
-  return next(newReq);
+  return next(req);
 };
--- src/app/services/tariff/tariff.service.ts
+++ src/app/services/tariff/tariff.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {TarifInterface} from "../../interfaces/tariff.interface";
 import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,10 @@ export class TariffService {
   constructor() { }
 
   getTariffs(): Observable<{tarifs: TarifInterface[]}> {
-    return this.http.get<{tarifs: TarifInterface[]}>('https://imbesmkaiakotvewnpnf.supabase.co/functions/v1/lefleuvecg');
+    return this.http.get<{tarifs: TarifInterface[]}>(environment.apiUrl);
   }
 
   getTariffById(id:string):Observable<{tarif: TarifInterface}> {
-    return this.http.get<{tarif: TarifInterface}>(`https://imbesmkaiakotvewnpnf.supabase.co/functions/v1/lefleuvecg/${id}`);
+    return this.http.get<{tarif: TarifInterface}>(`${environment.apiUrl}/${id}`);
   }
 }
