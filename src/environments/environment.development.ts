df85edea81a5f3ac0a430c4a7ca0ecef0e35cdb9
NgassakiJunescyPRATH <junescy.ngassaki@pratho.com>
2024-09-03 14:39:51 +0200

    added new files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://imbesmkaiakotvewnpnf.supabase.co/functions/v1/lefleuvecg'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://imbesmkaiakotvewnpnf.supabase.co/functions/v1/lefleuvecg'
+};
